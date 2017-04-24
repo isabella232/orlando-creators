b1e542ecb7e8d51d69eac57f50ba3bcea51e60e4
Drew Powers <drew@pow.rs>
2017-04-24 10:41:47 -0400

    Improve Facebook OG & Twitter tags.
    This leverages a good deal of metadata from YouTube, hopefully in the
    hopes of better shareability. Schema.org still largely missing, though.

--- pages/creator/clark-orr.js
+++ pages/creator/clark-orr.js
@@ -27,14 +27,37 @@ export default class extends React.Component {
         <main rel="main">
           <Head>
             <title>{pageTitle}</title>
-            <meta property="og:title" content={pageTitle} />
-            <meta property="og:url" content={`http://www.orlandocreators.com/creator/${creator.slug}`} />
             <meta property="og:image" content={`/static/assets/${creator.slug}-preview.jpg`} />
-            <meta property="twitter:image" content={`/static/assets/${creator.slug}-preview.jpg`} />
-            <meta property="twitter:site_name" content="http://www.orlandocreators.com" />
-            <meta property="twitter:site" content="@orlandocreators" />
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:description" content={creator.description} />
+            <meta property="og:type" content="video.episode" />
+            <meta property="og:url" content={`http://www.orlandocreators.com/creator/${creator.slug}`} />
+            <meta property="og:video" content={`https://www.youtube.com/watch?v=${creator.videoID}`} />
+            <meta property="og:video:actor" content={creator.name} />
+            <meta property="og:video:height" content="720" />
+            <meta property="og:video:release_date" content={creator.added} />
+            <meta property="og:video:secure_url" content={`https://www.youtube.com/embed/${creator.videoID}`} />
+            <meta property="og:video:series" content="Orlando Creators" />
+            <meta property="og:video:tag" content="adobe" />
+            <meta property="og:video:tag" content="artist" />
+            <meta property="og:video:tag" content="creative" />
+            <meta property="og:video:tag" content="design" />
+            <meta property="og:video:tag" content="dribbble" />
+            <meta property="og:video:tag" content="illustration" />
+            <meta property="og:video:tag" content="illustrator" />
+            <meta property="og:video:tag" content="interview" />
+            <meta property="og:video:tag" content="orlando" />
+            <meta property="og:video:tag" content="photoshop" />
+            <meta property="og:video:type" content="application/x-shockwave-flash" />
+            <meta property="og:video:type" content="text/html" />
+            <meta property="og:video:url" content={`https://www.youtube.com/embed/${creator.videoID}`} />
+            <meta property="og:video:width" content="1280" />
+            <meta name="twitter:image" content={`/static/assets/${creator.slug}-preview.jpg`} />
+            <meta name="twitter:site_name" content="http://www.orlandocreators.com" />
+            <meta name="twitter:site" content="@orlandocreators" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:description" content={creator.description} />
+            <meta name="twitter:player" content={`https://www.youtube.com/embed/${creator.videoID}`} />
+            <meta name="twitter:player:width" content="1280" />
+            <meta name="twitter:player:height" content="720" />
           </Head>
           <Header />
           <Creator
